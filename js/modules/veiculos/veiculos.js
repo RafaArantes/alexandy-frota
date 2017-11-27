784b7dfb8a008789cb0706feef019a502c1aed69
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-11-27 00:55:26 -0200

    Devmind commit in 2017-11-27 00:57:42

--- js/modules/veiculos/veiculos.js
+++ js/modules/veiculos/veiculos.js
@@ -14,48 +14,8 @@ var modulo = angular.module('main')
 		}
 
         $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/GetVeiculo') + '&callback=?', function(data){
-	    alert(data.contents);
+	        $scope.veiculos = data.contents;
         });
 
-		$scope.veiculos = [
-      {
-        "dtEntregaVenda": "2017-11-12T20:27:23.539",
-        "dtVenda": "2017-11-12T20:27:23.539",
-        "revendedor": "string",
-        "placa": "PAK0538",
-        "kmAtual": 0,
-        "fabricante": "string",
-        "modelo": "string",
-        "cor": "string",
-        "chassi": "string",
-        "renavam": "string",
-        "dtFabricacao": "2017-11-12T20:27:23.54",
-        "capacidadeTanque": 1,
-        "combustivelId": 1,
-        "contratoId": 1,
-        "statusVeiculoId": 1,
-        "tipoVeiculoId": 1,
-        "id": 2
-      },
-      {
-        "dtEntregaVenda": "2017-11-12T20:19:43.75",
-        "dtVenda": "2017-11-12T20:19:43.751",
-        "revendedor": "string",
-        "placa": "string",
-        "kmAtual": 0,
-        "fabricante": "string",
-        "modelo": "string",
-        "cor": "string",
-        "chassi": "string",
-        "renavam": "string",
-        "dtFabricacao": "2017-11-12T20:19:43.751",
-        "capacidadeTanque": 0,
-        "combustivelId": 1,
-        "contratoId": 1,
-        "statusVeiculoId": 1,
-        "tipoVeiculoId": 1,
-        "id": 1
-      }
-		]
 
 	});
