46cc3909710cd41c68a2661a1bb825618759a032
José Rafael <rafaell.u.a@hotmail.com>
2017-11-27 23:44:42 -0200

    Devmind commit in 2017-11-27 23:46:58

--- js/modules/ordemservico/ordemservico.js
+++ js/modules/ordemservico/ordemservico.js
@@ -111,8 +111,8 @@ var modulo = angular.module('main')
     $scope.ordemcadastro = {}; 
 
     $scope.send = function(){
-      $scope.ordemcadastro["unidadeId"] = $scope.selectedUnidade;
-      $scope.ordemcadastro["departamentoId"] = $scope.selectedDepartamento;   
+      $scope.unidade.id = $scope.selectedUnidade;
+      $scope.departamento.id = $scope.selectedDepartamento;   
       console.log($scope.ordemcadastro); 
     }
 	$scope.cadastrarOs = function() {
