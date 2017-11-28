9263617da0805d42903007c273b816ed0fd9c05f
conilas <nick.meinen@live.com>
2017-11-27 22:24:32 -0200

    fucking rafael'

--- js/modules/ordemservico/ordemservico.js
+++ js/modules/ordemservico/ordemservico.js
@@ -13,46 +13,9 @@ var modulo = angular.module('main')
 		$scope.setUserPage = function(){
 			$scope.pag = 1;
 		}
-		$scope.veiculoObj = [
-      {
-        "dtEntregaVenda": "2017-11-12T20:27:23.539",
-        "dtVenda": "2017-11-12T20:27:23.539",
-        "revendedor": "string",
-        "placa": "PAK0538",
-        "kmAtual": 0,
-        "fabricante": "string",
-        "modelo": "string",
-        "cor": "string",
-        "chassi": "string",
-        "renavam": "string",
-        "dtFabricacao": "2017-11-12T20:27:23.54",
-        "capacidadeTanque": 1,
-        "combustivelId": 1,
-        "contratoId": 1,
-        "statusVeiculoId": 1,
-        "tipoVeiculoId": 1,
-        "id": 2
-      },
-      {
-        "dtEntregaVenda": "2017-11-12T20:19:43.75",
-        "dtVenda": "2017-11-12T20:19:43.751",
-        "revendedor": "string",
-        "placa": "string",
-        "kmAtual": 0,
-        "fabricante": "string",
-        "modelo": "string",
-        "cor": "string",
-        "chassi": "string",
-        "renavam": "string",
-        "dtFabricacao": "2017-11-12T20:19:43.751",
-        "capacidadeTanque": 0,
-        "combustivelId": 1,
-        "contratoId": 1,
-        "statusVeiculoId": 1,
-        "tipoVeiculoId": 1,
-        "id": 1
-      }
-		]
+
+		$scope.veiculoObj = []
+
 		$scope.motoristaObj = [
       {
         "nome": "string",
@@ -102,7 +65,7 @@ var modulo = angular.module('main')
       }
     ]
 		$scope.statusordem = [
-      {
+      { 
         "nome": "Agendada",
         "id": 1
       },
@@ -115,106 +78,71 @@ var modulo = angular.module('main')
         "id": 3
       }
     ]
-		$scope.ordemservico = [
+		$scope.ordemservico = []; 
+		
+    var usuario = [
       {
-        "dtSolicitacao": "2017-11-12T20:19:42.994",
-        "passageiro": "string",
-        "dtSaida": "2017-11-12T20:19:42.994",
-        "enderecoDestino": "string",
-        "finalidadeServido": "string",
-        "localSaida": "string",
-        "dtChegada": "2017-11-12T20:19:42.994",
-        "kmInicial": 0,
-        "kmFinal": 0,
-        "arquivo": "string",
-        "statusOrdemServicoId": 1,
-        "unidadeId": 1,
-        "motoristaId": 1,
-        "veiculoId": 1,
-        "userSolicitacaoId": 1,
-        "userConfirmacaoId": 1,
-        "userBaixaId": 1,
-        "id": 10
-      },
-      {
-        "dtSolicitacao": "2017-11-12T20:19:42.994",
-        "passageiro": "string",
-        "dtSaida": "2017-11-12T20:19:42.994",
-        "enderecoDestino": "string",
-        "finalidadeServido": "string",
-        "localSaida": "string",
-        "dtChegada": "2017-11-12T20:19:42.994",
-        "kmInicial": 0,
-        "kmFinal": 0,
-        "arquivo": "string",
-        "statusOrdemServicoId": 1,
-        "unidadeId": 1,
-        "motoristaId": 2,
-        "veiculoId": 2,
-        "userSolicitacaoId": 1,
-        "userConfirmacaoId": 1,
-        "userBaixaId": 1,
-        "id": 11
-      },
-      {
-        "dtSolicitacao": "2017-11-12T20:19:42.994",
-        "passageiro": "string",
-        "dtSaida": "2017-11-12T20:19:42.994",
-        "enderecoDestino": "string",
-        "finalidadeServido": "string",
-        "localSaida": "string",
-        "dtChegada": "2017-11-12T20:19:42.994",
-        "kmInicial": 0,
-        "kmFinal": 0,
-        "arquivo": "string",
-        "statusOrdemServicoId": 1,
-        "unidadeId": 1,
-        "motoristaId": 2,
-        "veiculoId": 2,
-        "userSolicitacaoId": 1,
-        "userConfirmacaoId": 1,
-        "userBaixaId": 1,
-        "id": 12
+        "userName": "string",
+        "name": "string",
+        "surname": "string",
+        "emailAddress": "string",
+        "isActive": true,
+        "fullName": "string",
+        "lastLoginTime": "2017-11-26T20:21:18.826Z",
+        "creationTime": "2017-11-26T20:21:18.826Z",
+        "roleNames": [
+          "string"
+        ],
+        "id": 1
       }
     ]
-		var usuario = [
-	    {
-	      "userName": "string",
-	      "name": "string",
-	      "surname": "string",
-	      "emailAddress": "string",
-	      "isActive": true,
-	      "fullName": "string",
-	      "lastLoginTime": "2017-11-26T20:21:18.826Z",
-	      "creationTime": "2017-11-26T20:21:18.826Z",
-	      "roleNames": [
-	        "string"
-	      ],
-	      "id": 1
-	    }
-  	]
-		for(var i = 0; i < $scope.ordemservico.length; i++){
-			var motorista_id = $scope.ordemservico[i].motoristaId;
-			var veiculo_id = $scope.ordemservico[i].veiculoId;
-			var usersolicitacao_Id = $scope.ordemservico[i].userSolicitacaoId;
-			var unidade_Id = $scope.ordemservico[i].unidadeId;
 
-			var unidadeobj = usuario.find(item => item.id === unidade_Id);
-			var motoristaobject = $scope.motoristaObj.find(item => item.id === motorista_id);
-			var veiculoidobject = $scope.veiculoObj.find(item => item.id === veiculo_id);
-			var usersolidobject = usuario.find(item => item.id === usersolicitacao_Id);
+    $scope.send = function(){
+      console.log($scope.ordem); 
+    }
 
-			console.log(unidadeobj)
-			$scope.ordemservico[i].motorista = motoristaobject;
-			$scope.ordemservico[i].veiculo = veiculoidobject;
-		};
 		$scope.editarOs = function(os){
 			var osc = $scope.ordemservico;
-			osc.placa = os.veiculo.placa;
+			osc.placa = os.veiculo.placa;  
 			osc.id = os.id;
 			osc.dtSaida = os.dtSaida;
 			osc.dtSolicitacao = os.dtSolicitacao;
-		}
+		} 
+
+    $scope.loadOrdemServico = function(){
+      $http.get("http://frotasystembackend.azurewebsites.net/api/services/app/OrdemServico/GetOrdemServico").then(response => {
+        $scope.ordemservico = response.data.result.items;
+            for(var i = 0; i < $scope.ordemservico.length; i++){
+                var motorista_id = $scope.ordemservico[i].motoristaId; 
+                var veiculo_id = $scope.ordemservico[i].veiculoId;
+                var usersolicitacao_Id = $scope.ordemservico[i].userSolicitacaoId;
+                var unidade_Id = $scope.ordemservico[i].unidadeId;
+
+                var unidadeobj = usuario.find(item => item.id === unidade_Id);
+                var motoristaobject = $scope.motoristaObj.find(item => item.id === motorista_id);
+                var veiculoidobject = $scope.veiculoObj.find(item => item.id === veiculo_id); 
+                var usersolidobject = usuario.find(item => item.id === usersolicitacao_Id);
+
+                console.log(unidadeobj)
+                $scope.ordemservico[i].motorista = motoristaobject;
+                $scope.ordemservico[i].veiculo = veiculoidobject;
+          };   
+      }) 
+    }
+
+    $scope.loadVeiculos = function(){
+      $http.get("http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/GetVeiculo").then(response => {
+          $scope.veiculoObj = response.data.result.items;
+          $scope.loadOrdemServico(); 
+      });
+    }
+
+    $scope.loadAll = function(){
+      $scope.loadVeiculos();
+    }
+
+    $scope.loadAll();
+
 		$scope.clearCamps = function() {
       $scope.ordemservico.ordemServicoForm = "";
 		}
