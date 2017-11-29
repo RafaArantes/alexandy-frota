93bdafe02d4993beb077caf91d6c977000473dca
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-11-29 03:56:33 -0200

    Devmind commit in 2017-11-29 03:58:50

--- js/modules/veiculos/veiculos.js
+++ js/modules/veiculos/veiculos.js
@@ -1,99 +1,112 @@
-var modulo = angular.module('main')
+var modulo = angular
+  .module("main")
 
-.controller('controllerVeiculos', function($location, $scope, $timeout, $http)
-{
-
-    console.log('Veiculos controller');
+  .controller("controllerVeiculos", function(
+    $location,
+    $scope,
+    $timeout,
+    $http
+  ) {
+    console.log("Veiculos controller");
 
     //Entities to cadastrate
     $scope.veiculo = {};
 
-    $scope.send = function()
-    {
-        console.log($scope.veiculo);
-        
-        $http({
-          method: 'POST',
-          data: $scope.veiculo,
-          contentType: "application/json; charset=utf-8",
-          url: 'http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/CreateVeiculoAsync'
-        }).then(function successCallback(response) {
-            
-          }, function errorCallback(response) {
-          });
-        
-    }
+    $scope.send = function() {
+      console.log($scope.veiculo);
 
-    $.getJSON('http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/GetVeiculo', function(data)
-    {
+      $http.post(
+        "http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/CreateVeiculoAsync",
+        $scope.veiculo,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+          }
+        }
+      );
+    };
 
+    $.getJSON(
+      "http://frotasystembackend.azurewebsites.net/api/services/app/Veiculo/GetVeiculo",
+      function(data) {
         $scope.veiculos = [];
-        
-        $(data['result']['items']).each(function(index, object)
-        {
 
-            $scope.veiculos.push(object);
-            $scope.$apply();
-
+        $(data["result"]["items"]).each(function(index, object) {
+          $scope.veiculos.push(object);
+          $scope.$apply();
         });
+      }
+    );
 
-    });
-
-    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://frotasystembackend.azurewebsites.net/api/services/app/Combustivel/GetCombustiveis') + '&callback=?', function(data)
-    {
-
+    $.getJSON(
+      "http://www.whateverorigin.org/get?url=" +
+        encodeURIComponent(
+          "http://frotasystembackend.azurewebsites.net/api/services/app/Combustivel/GetCombustiveis"
+        ) +
+        "&callback=?",
+      function(data) {
         $scope.combustiveis = [];
-        $(JSON.parse(data.contents)['result']['items']).each(function(index, object)
-        {
-
-            $scope.combustiveis.push(object);
-            $scope.$apply();
-
+        $(JSON.parse(data.contents)["result"]["items"]).each(function(
+          index,
+          object
+        ) {
+          $scope.combustiveis.push(object);
+          $scope.$apply();
         });
+      }
+    );
 
-    });
-
-    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://frotasystembackend.azurewebsites.net/api/services/app/Contrato/GetContrato') + '&callback=?', function(data)
-    {
-
+    $.getJSON(
+      "http://www.whateverorigin.org/get?url=" +
+        encodeURIComponent(
+          "http://frotasystembackend.azurewebsites.net/api/services/app/Contrato/GetContrato"
+        ) +
+        "&callback=?",
+      function(data) {
         $scope.contratos = [];
-        $(JSON.parse(data.contents)['result']['items']).each(function(index, object)
-        {
-
-            $scope.contratos.push(object);
-            $scope.$apply();
-
+        $(JSON.parse(data.contents)["result"]["items"]).each(function(
+          index,
+          object
+        ) {
+          $scope.contratos.push(object);
+          $scope.$apply();
         });
+      }
+    );
 
-    });
-
-    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://frotasystembackend.azurewebsites.net/api/services/app/TipoVeiculo/GetTipoVeiculo') + '&callback=?', function(data)
-    {
-
+    $.getJSON(
+      "http://www.whateverorigin.org/get?url=" +
+        encodeURIComponent(
+          "http://frotasystembackend.azurewebsites.net/api/services/app/TipoVeiculo/GetTipoVeiculo"
+        ) +
+        "&callback=?",
+      function(data) {
         $scope.tiposveiculo = [];
-        $(JSON.parse(data.contents)['result']['items']).each(function(index, object)
-        {
-
-            $scope.tiposveiculo.push(object);
-            $scope.$apply();
-
+        $(JSON.parse(data.contents)["result"]["items"]).each(function(
+          index,
+          object
+        ) {
+          $scope.tiposveiculo.push(object);
+          $scope.$apply();
         });
+      }
+    );
 
-    });
-    
-     $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://frotasystembackend.azurewebsites.net/api/services/app/StatusVeiculo/GetStatusVeiculo') + '&callback=?', function(data)
-    {
-
+    $.getJSON(
+      "http://www.whateverorigin.org/get?url=" +
+        encodeURIComponent(
+          "http://frotasystembackend.azurewebsites.net/api/services/app/StatusVeiculo/GetStatusVeiculo"
+        ) +
+        "&callback=?",
+      function(data) {
         $scope.statusveiculos = [];
-        $(JSON.parse(data.contents)['result']['items']).each(function(index, object)
-        {
-
-            $scope.statusveiculos.push(object);
-            $scope.$apply();
-
+        $(JSON.parse(data.contents)["result"]["items"]).each(function(
+          index,
+          object
+        ) {
+          $scope.statusveiculos.push(object);
+          $scope.$apply();
         });
-
-    });
-    
-
-});
+      }
+    );
+  });
